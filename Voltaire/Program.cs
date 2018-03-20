ce64abd19f99ba800a18a976b87322becedbbe8c
Noel Minchow <noelminchow@gmail.com>
2018-03-20 00:29:57 -0500

    Add project files.

---
+++ Voltaire/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Voltaire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
