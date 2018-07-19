47186df41a6875837cd9a46551a7427057c91747
nminchow <noelminchow@gmail.com>
2018-07-18 23:08:30 -0500

    more dm debugging

--- Voltaire/Controllers/Messages/SendDirectMessage.cs
+++ Voltaire/Controllers/Messages/SendDirectMessage.cs
@@ -14,12 +14,12 @@ namespace Voltaire.Controllers.Messages
         {
             userName = userName.StartsWith('@') ? userName.Substring(1) : userName;
 
-
             Console.WriteLine("Starting DM Query");
-            var user = currentContext.Client.Guilds
-                .Where(x => x.Users.ToLookup(u => u.Id)[currentContext.User.Id] != null)
-                .Aggregate(new List<SocketGuildUser>(), (acc, item) => acc.Concat(item.Users).ToList())
-                .FirstOrDefault(x => (x.Username.ToLower() == userName.ToLower() || x.Id.ToString() == userName) && !x.IsBot);
+            var guildList = currentContext.Client.Guilds.Where(x => x.Users.ToLookup(u => u.Id)[currentContext.User.Id] != null);
+            Console.WriteLine("Got guilds");
+            var userList = guildList.Aggregate(new List<SocketGuildUser>(), (acc, item) => acc.Concat(item.Users).ToList());
+            Console.WriteLine("Got users");
+            var user = userList.FirstOrDefault(x => (x.Username.ToLower() == userName.ToLower() || x.Id.ToString() == userName) && !x.IsBot);
             Console.WriteLine("Completed DM Query");
             if (user == null)
             {
