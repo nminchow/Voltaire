a79841662357914318c4de624700d94160045fce
nminchow <noelminchow@gmail.com>
2018-07-24 22:20:06 -0500

    more debugging and entity framework

--- Voltaire/Controllers/Messages/SendDirectMessage.cs
+++ Voltaire/Controllers/Messages/SendDirectMessage.cs
@@ -13,23 +13,31 @@ namespace Voltaire.Controllers.Messages
         public static async Task PerformAsync(SocketCommandContext currentContext, string userName, string message)
         {
             userName = userName.StartsWith('@') ? userName.Substring(1) : userName;
-
-            Console.WriteLine("Starting DM Query");
-            var guildList = currentContext.Client.Guilds.Where(x => x.Users.ToLookup(u => u.Id)[currentContext.User.Id] != null);
-            Console.WriteLine("Got guilds");
-            var userList = guildList.Aggregate(new List<SocketGuildUser>(), (acc, item) => acc.Concat(item.Users).ToList());
-            Console.WriteLine("Got users");
-            var user = userList.FirstOrDefault(x => (x.Username.ToLower() == userName.ToLower() || x.Id.ToString() == userName) && !x.IsBot);
-            Console.WriteLine("Completed DM Query");
-            if (user == null)
+            try
             {
-                await currentContext.Channel.SendMessageAsync("user not found");
-                return;
-            }
+                Console.WriteLine("Starting DM Query");
+                var guildList = currentContext.Client.Guilds.Where(x => x.Users.ToLookup(u => u.Id)[currentContext.User.Id] != null);
+                Console.WriteLine("Got guilds");
+                var userList = guildList.Aggregate(new List<SocketGuildUser>(), (acc, item) => acc.Concat(item.Users).ToList());
+                Console.WriteLine("Got users");
+                Console.WriteLine(userList.Count());
+                var user = userList.FirstOrDefault(x => (x.Username.ToLower() == userName.ToLower() || x.Id.ToString() == userName) && !x.IsBot);
+                Console.WriteLine("Completed DM Query");
+                if (user == null)
+                {
+                    await currentContext.Channel.SendMessageAsync("user not found");
+                    return;
+                }
 
-            var userChannel = await user.GetOrCreateDMChannelAsync();
-            await userChannel.SendMessageAsync($"an anonymous user says: {message}");
-            await currentContext.Channel.SendMessageAsync("Sent!");
+                var userChannel = await user.GetOrCreateDMChannelAsync();
+                await userChannel.SendMessageAsync($"an anonymous user says: {message}");
+                await currentContext.Channel.SendMessageAsync("Sent!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            
         }
     }
 }
---
+++ Voltaire/Models/Guild.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Voltaire.Models
+{
+    class Guild
+    {
+        public int ID { get; set; }
+        public string DiscordId { get; set; }
+    }
+}
