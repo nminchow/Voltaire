1f021be4fb0e756c7195b7e8b4414021429c3ab5
nminchow <noelminchow@gmail.com>
2021-02-23 18:10:56 -0500

    example

--- Voltaire/Controllers/Helpers/IncrementAndCheckMessageLimit.cs
+++ Voltaire/Controllers/Helpers/IncrementAndCheckMessageLimit.cs
@@ -14,7 +14,8 @@ namespace Voltaire.Controllers.Helpers
             guild.MessagesSentThisMonth += 1;
             db.SaveChanges();
 
-            return guild.MessagesSentThisMonth <= 50 || EnsureActiveSubscription.Perform(guild, db);
+            return true;
+            // return guild.MessagesSentThisMonth <= 50 || EnsureActiveSubscription.Perform(guild, db);
         }
 
         public static void CheckMonth(Guild guild)
