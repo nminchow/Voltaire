919bf2515160d435e078aa9d2a1afbc647245ac4
nminchow <noelminchow@gmail.com>
2022-03-05 23:31:52 -0600

    add subscription slash commands

--- Voltaire/Modules/InteractionsBase.cs
+++ Voltaire/Modules/InteractionsBase.cs
@@ -8,6 +8,7 @@ namespace Voltaire.Modules
     {
         protected DataBase _database;
         protected Func<string, Discord.Embed, Task> Responder;
+        protected Func<string, Discord.Embed, Task> PublicResponder;
 
         public InteractionsBase(DataBase database)
         {
@@ -18,6 +19,14 @@ namespace Voltaire.Modules
                     return RespondAsync(response, new[] { embed }, ephemeral: true);
                 }
                 return RespondAsync(response, ephemeral: true);
+            };
+
+            PublicResponder = (response, embed) =>
+            {
+                if (embed != null)  {
+                    return RespondAsync(response, new[] { embed }, ephemeral: false);
+                }
+                return RespondAsync(response, ephemeral: false);
             };
         }
     }
---
+++ Voltaire/Modules/SubscriptionInteractions.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Discord.Interactions;
+using Discord;
+
+namespace Voltaire.Modules
+{
+    public class SubscriptionInteractions : InteractionsBase
+    {
+
+        public SubscriptionInteractions(DataBase database): base(database) {}
+
+
+        [SlashCommand("pro", "generate a link to upgrade to voltaire pro or get current subscription info")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task Pro()
+        {
+            await Controllers.Subscriptions.Pro.PerformAsync(new InteractionBasedContext(Context, PublicResponder), _database);
+        }
+
+        [SlashCommand("pro_cancel", "cancel your voltaire pro subscription")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task Cancel()
+        {
+            await Controllers.Subscriptions.Cancel.PerformAsync(new InteractionBasedContext(Context, PublicResponder), _database);
+        }
+    }
+}
