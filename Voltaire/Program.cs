e72ae58fd5d8ed09b5bef17968927d6d82781e5d
nminchow <noelminchow@gmail.com>
2022-03-12 22:12:32 -0600

    use new help command for status

--- Voltaire/Program.cs
+++ Voltaire/Program.cs
@@ -66,7 +66,7 @@ namespace Voltaire
             await InstallCommandsAsync();
 
             await _client.LoginAsync(TokenType.Bot, token);
-            await _client.SetGameAsync("!volt help");
+            await _client.SetGameAsync("/volt-help");
 
             await _client.StartAsync();
 
